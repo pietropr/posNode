64699af58126ba635a352cdcb48923d0187d7784
pietropr <pintonpietro@gmail.com>
2018-08-22 18:50:45 -0300

    Adicionando Statics

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var rotaUsuario = require('./usuarios.js');
 
 // Rota para usuários
 app.use('/usuarios', rotaUsuario);
+//Rota estilos
+app.use('/assets', express.static('./assets'));
+
+app.get('/', function(req, res) {
+	res.send('<link rel="stylesheet" href="assets/app.css"><h3>Bem vindo ao primeiro projeto Node</h3>');
+});
 
 app.listen(3000);
 console.log('Servidor iniciado na porta 3000');
