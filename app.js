b5e66cc837ccecdc814305ec6eab090e81fa52bc
pietropr <pintonpietro@gmail.com>
2018-08-22 19:08:01 -0300

    Primeira View do padrão Model View Controller (MVC)

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var express = require('express');
 var app = express();
 var rotaUsuario = require('./usuarios.js');
 
+//Set permite configurar head HTTP
+app.set('view engine', 'ejs');
+
+//HOME - O render, diferente do 'send', chama um template da pasta views
+app.get('/', function(req, res) {
+	res.render('principal/index', {nome: 'Pietro', sobrenome: 'Pinton'});
+});
+
 // Rota para usuários
 app.use('/usuarios', rotaUsuario);
 //Rota estilos
 app.use('/assets', express.static('./assets'));
 
-app.get('/', function(req, res) {
-	res.send('<link rel="stylesheet" href="assets/app.css"><h3>Bem vindo ao primeiro projeto Node</h3>');
-});
 
 app.listen(3000);
 console.log('Servidor iniciado na porta 3000');
